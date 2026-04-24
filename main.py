e646fea4051447c1687888fe22bbff31832c97b5
flaviamedici <flaviaomedici@gmail.com>
2026-04-24 15:20:31 -0700

    Adding sending email capabilities

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+import ssl
+
 import requests
 import selectorlib
+#import smtplib, ssl
+#import os
 
 URL = "https://www.events12.com/seattle/"
 HEADERS = {
@@ -24,6 +29,22 @@ def extract(source):
         cleaned_data = "\n".join([line.strip() for line in value[2:] if line.strip()])
         return cleaned_data
     return ""
+
+"""
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    
+    username = ""
+    password = ""
+    
+    receiver = "app8flask@gmail.com"
+    context = ssl.create_default_context()
+    
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+"""
 
 def send_email():
     print("Sending email...")
