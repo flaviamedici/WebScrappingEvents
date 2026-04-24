382d80be532b478b50c43beac55bf14e1c7fe9be
flaviamedici <flaviaomedici@gmail.com>
2026-04-24 14:17:18 -0700

    Added the extract and store in a txt file and added structure to the information being scrapped

--- main.py
+++ main.py
@@ -12,13 +12,37 @@ def scrape(url):
     source = response.text
     return source
 
+
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extractor.yaml")
     value = extractor.extract(source)["concerts"]
-    return value
+
+    # Filter out headers or empty rows and clean up whitespace
+    # This joins the list into a single string with line breaks
+    if value:
+        # We skip the first two rows because they are the "Concerts" header and the Image
+        cleaned_data = "\n".join([line.strip() for line in value[2:] if line.strip()])
+        return cleaned_data
+    return ""
+
+def send_email():
+    print("Sending email...")
+
+def store(extracted):
+    with open("data.txt", "w", encoding="utf-8") as file:
+        file.write(extracted)
+
+def read():
+    with open("data.txt", "r", encoding="utf-8") as file:
+        return file.read()
 
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+
+    if extracted != "No upcoming events":
+        if extracted not in "data.txt":
+            store(extracted)
+            send_email()
