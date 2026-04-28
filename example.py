028f62558aab8f3c12a12ad283190a3fb2e82c0a
flaviamedici <flaviaomedici@gmail.com>
2026-04-27 19:18:11 -0700

    Adding some examples for using a sql database in python

--- example.py
+++ example.py
@@ -5,6 +5,19 @@ import sqlite3
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM concerts WHERE Band='Anyma'")
+#query al data
+cursor.execute('SELECT * FROM concerts WHERE Venue="Climate Pledge Arena"')
 rows = cursor.fetchall()
 print(rows)
+
+#query certain columns
+cursor.execute('SELECT Band, Venue FROM concerts')
+rows = cursor.fetchall()
+print(rows)
+
+#Insert new rows
+new_rows = [("Hozier", "August 20th", "Gorge"),
+            ("Tame Impala", "May 9th", "Lumen Field")]
+
+cursor.executemany("INSERT INTO concerts VALUES (?,?,?)", new_rows)
+connection.commit()
